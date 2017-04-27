12007579b2c618a6132981706aa70cd60d52174c
Rustam Gaifullin <rustam.gaifullin@gmail.com>
2017-04-27 21:19:47 +0200

    comments to null safety.

--- src/main/kotlin/com/rustam/features/7.NullSafety.kt
+++ src/main/kotlin/com/rustam/features/7.NullSafety.kt
@@ -13,20 +13,20 @@ fun main(args: Array<String>) {
     val someString = SomeString()
 
     val nullableString = someString.nullableString()
-    println(nullableString)
-    println(nullableString?.substring(1))
+    println(nullableString) //print null
+    println(nullableString?.substring(1)) //print also null
 
     val normalString = someString.normalString()
-    println(normalString?.substring(1))
+    println(normalString?.substring(1)) //print b
 
     if (nullableString != null) {
-        println(nullableString.substring(0))
+        println(nullableString.substring(0)) //no need to use safe call
     }
 
     val subString = nullableString?.substring(0) ?: "c"
-    println(subString)
+    println(subString) //print c
 
 
-    nullableString?.let {println(it)}
-    normalString?.let { println(it) }
+    nullableString?.let {println(it)} //print nothing
+    normalString?.let { println(it) } //print ab
 }
